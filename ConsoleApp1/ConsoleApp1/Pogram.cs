1164c8d1c6971cd26fc3c856f6e0569df16e88ec
Aria Findlay <freddie.findlay@outlook.com>
2022-08-05 12:27:47 +0100

    finished day 3

--- ConsoleApp1/ConsoleApp1/Pogram.cs
+++ ConsoleApp1/ConsoleApp1/Pogram.cs
@@ -1,6 +1,9 @@
-﻿Numbers();
+﻿BasicLists();
+
+Numbers();
 
 BranchesAndLoops();
+
 
 
 #region Branches and loops
@@ -19,7 +22,7 @@ void SumStreamOfIntegersDivisibleBy(int min, int max, int divisor)
     int sum = 0;
     for (int index = min; index < max + 1; index++)
     {
-        if (index %divisor == 0)
+        if (index % divisor == 0)
         {
             sum = sum + index;
         }
@@ -98,133 +101,235 @@ void ExploreLoops()
 #region Numbers
 
 void Numbers()
+{
+    WorkWithIntegers();
+
+    OrderPrecedence();
+
+    TestLimits();
+
+    WorkWithDoubles();
+
+    WorkingWithDecimals();
+
+    CalculateAreaOfAUserDefinedCircle();
+}
+
+void WorkWithIntegers()
+{
+    int a = 18;
+    int b = 6;
+    int c = a + b;
+    Console.WriteLine(c);
+
+    // subtraction
+    c = a - b;
+    Console.WriteLine(c);
+
+    //multiplication
+    c = a * b;
+    Console.WriteLine(c);
+
+    //division
+    c = a / b;
+    Console.WriteLine(c);
+}
+
+void OrderPrecedence()
+{
+    int a = 5;
+    int b = 4;
+    int c = 2;
+    int d = (a + b) * c;
+    Console.WriteLine(d);
+
+    d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+    Console.WriteLine(d);
+
+    int e = 7;
+    int f = 4;
+    int g = 3;
+    int h = (e + f) / g;
+    Console.WriteLine(h);
+}
+
+void TestLimits()
+{
+    int a = 7;
+    int b = 4;
+    int c = 3;
+    int d = (a + b) / c;
+    int e = (a + b) % c;
+    Console.WriteLine($"quotient: {d}");
+    Console.WriteLine($"remainder: {e}");
+
+    int max = int.MaxValue;
+    int min = int.MinValue;
+    Console.WriteLine($"the range of integers is {min} to {max}");
+
+    int what = max + 3;
+    Console.WriteLine($"an example of overflow: {what}");
+}
+
+void WorkWithDoubles()
+{
+    double a = 5;
+    double b = 4;
+    double c = 2;
+    double d = (a + b) / c;
+    Console.WriteLine(d);
+
+    double e = 19;
+    double f = 23;
+    double g = 8;
+    double h = (e + f) / g;
+    Console.WriteLine(h);
+
+    double max = double.MaxValue;
+    double min = double.MinValue;
+    Console.WriteLine($"the range of double is {min} to {max}");
+
+    double third = 1.0 / 3.0;
+    Console.WriteLine(third);
+}
+
+void WorkingWithDecimals()
+{
+    decimal min = decimal.MinValue;
+    decimal max = decimal.MaxValue;
+    Console.WriteLine($"the range of the decimal type is {min} to {max}");
+
+    double a = 1.0;
+    double b = 3.0;
+    Console.WriteLine(a / b);
+
+    decimal c = 1.0M;
+    decimal d = 3.0M;
+    Console.WriteLine(c / d);
+}
+
+void CalculateAreaOfACircle(double radius)
+{
+    double area = radius * radius * Math.PI;
+    Console.WriteLine($"The area of a circle with a radius {radius} is {area}");
+}
+
+void CalculateAreaOfAUserDefinedCircle()
+{
+    Console.WriteLine("Enter radius:");
+    string input = Console.ReadLine();
+    double rad;
+    bool ok = Double.TryParse(input, out rad);
+    if (!ok)
     {
-        WorkWithIntegers();
+        Console.WriteLine($"This is not a number: {input}");
+    }
+    else
+    {
+        CalculateAreaOfACircle(rad);
+    }
+}
 
-        OrderPrecedence();
+static void NewMethod()
+{
+    var names = new List<string> { "aria", "ana", "felipe" };
+    Console.WriteLine();
+    names.Add("Maria");
+    names.Add("Bill");
+    names.Remove("ana");
+    foreach (var name in names)
 
-        TestLimits();
+    {
+        Console.WriteLine($"hello {name.ToUpper()}!");
+    }
+    Console.WriteLine($"my name is {names[0]}");
+    Console.WriteLine($"I've added {names[2]} and {names[3]} to the list");
+    Console.WriteLine($"The list has {names.Count} people in it");
+}
 
-        WorkWithDoubles();
+#endregion
 
-        WorkingWithDecimals();
+#region
 
-        CalculateAreaOfAUserDefinedCircle();
+void BasicLists()
+{
+    WorkWithStrings();
+
+    Fibonacci();
+
+    challenge();
+}
+
+void WorkWithStrings()
+{
+    var names = new List<string> { "Aria", "Ana", "Felipe" };
+    foreach (var name in names)
+    {
+        Console.WriteLine($"Hello {name.ToUpper()}!");
+    }
+    Console.WriteLine();
+    names.Add("Maria");
+    names.Add("Bill");
+    names.Remove("Ana");
+    foreach (var name in names)
+    {
+        Console.WriteLine($"Hello {name.ToUpper()}!");
+    }
+    Console.WriteLine($"My name is {names[0]}");
+    Console.WriteLine($"I've added {names[2]} and {names[3]} to the list");
+    Console.WriteLine($"The list has {names.Count} people in it");
+    var index = names.IndexOf("Felipe");
+    if (index == -1)
+    {
+        Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+    }
+    else
+    {
+        Console.WriteLine($"The name {names[index]} is at index {index}");
     }
 
-    void WorkWithIntegers()
+    index = names.IndexOf("Not Found");
+    if (index == -1)
     {
-        int a = 18;
-        int b = 6;
-        int c = a + b;
-        Console.WriteLine(c);
-
-        // subtraction
-        c = a - b;
-        Console.WriteLine(c);
-
-        //multiplication
-        c = a * b;
-        Console.WriteLine(c);
-
-        //division
-        c = a / b;
-        Console.WriteLine(c);
+        Console.WriteLine($"When an item is not found, IndexOf returns {index}");
     }
-
-    void OrderPrecedence()
+    else
     {
-        int a = 5;
-        int b = 4;
-        int c = 2;
-        int d = (a + b) * c;
-        Console.WriteLine(d);
+        Console.WriteLine($"The name {names[index]} is at index {index}");
 
-        d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
-        Console.WriteLine(d);
-
-        int e = 7;
-        int f = 4;
-        int g = 3;
-        int h = (e + f) / g;
-        Console.WriteLine(h);
     }
-
-    void TestLimits()
+    names.Sort();
+    foreach (var name in names)
     {
-        int a = 7;
-        int b = 4;
-        int c = 3;
-        int d = (a + b) / c;
-        int e = (a + b) % c;
-        Console.WriteLine($"quotient: {d}");
-        Console.WriteLine($"remainder: {e}");
-
-        int max = int.MaxValue;
-        int min = int.MinValue;
-        Console.WriteLine($"the range of integers is {min} to {max}");
-
-        int what = max + 3;
-        Console.WriteLine($"an example of overflow: {what}");
+        Console.WriteLine($"Hello {name.ToUpper()}!");
     }
+}
 
-    void WorkWithDoubles()
+void Fibonacci()
+{
+    var fibonacciNumbers = new List<int> { 1, 1 };
+    var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+    var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+    fibonacciNumbers.Add(previous + previous2);
+
+    foreach (var item in fibonacciNumbers)
+        Console.WriteLine(item);
+}
+
+void challenge()
+{
+    var fibonacciNumbers = new List<int> { 1, 1 };
+    while (fibonacciNumbers.Count < 20)
     {
-        double a = 5;
-        double b = 4;
-        double c = 2;
-        double d = (a + b) / c;
-        Console.WriteLine(d);
+        var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+        var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
 
-        double e = 19;
-        double f = 23;
-        double g = 8;
-        double h = (e + f) / g;
-        Console.WriteLine(h);
-
-        double max = double.MaxValue;
-        double min = double.MinValue;
-        Console.WriteLine($"the range of double is {min} to {max}");
-
-        double third = 1.0 / 3.0;
-        Console.WriteLine(third);
+        fibonacciNumbers.Add(previous + previous2);
     }
+    foreach (var item in fibonacciNumbers)
+        Console.WriteLine(item);
+}
 
-    void WorkingWithDecimals()
-    {
-        decimal min = decimal.MinValue;
-        decimal max = decimal.MaxValue;
-        Console.WriteLine($"the range of the decimal type is {min} to {max}");
-
-        double a = 1.0;
-        double b = 3.0;
-        Console.WriteLine(a / b);
-
-        decimal c = 1.0M;
-        decimal d = 3.0M;
-        Console.WriteLine(c / d);
-    }
-
-    void CalculateAreaOfACircle(double radius)
-    {
-        double area = radius * radius * Math.PI;
-        Console.WriteLine($"The area of a circle with a radius {radius} is {area}");
-    }
-
-    void CalculateAreaOfAUserDefinedCircle()
-    {
-        Console.WriteLine("Enter radius:");
-        string input = Console.ReadLine();
-        double rad;
-        bool ok = Double.TryParse(input, out rad);
-        if (!ok)
-        {
-            Console.WriteLine($"This is not a number: {input}");
-        }
-        else
-        {
-            CalculateAreaOfACircle(rad);
-        }
-    }
 
 #endregion
